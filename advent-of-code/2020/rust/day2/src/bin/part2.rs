cd05d5b51d2c02c2f47a0b7bc26ed07b7211ad82
Paul Mabileau <paulmabileau@hotmail.fr>
2020-12-14 19:12:19 +0100

    AoC D2 - Part 2
    Not a very interesting exercise after all...
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

---
+++ advent-of-code/2020/rust/day2/src/bin/part2.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+use std::io;
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+    let mut line = String::new();
+    let mut count = 0;
+
+    while stdin.read_line(&mut line)? != 0 {
+        let parts = line.split_whitespace().collect::<Vec<_>>();
+
+        let letter = parts[1].as_bytes()[0];
+        let pswd = parts[2].as_bytes();
+
+        count += (parts[0]
+            .splitn(2, '-')
+            .map(|s| s.parse::<usize>().unwrap())
+            .filter(|&n| pswd[n - 1] == letter)
+            .count()
+            == 1) as u16;
+
+        line.clear();
+    }
+
+    println!("{}", count);
+    Ok(())
+}
