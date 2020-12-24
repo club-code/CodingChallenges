fedb8436817cf9ab26d3c73f0c67e2968b5c364f
Paul Mabileau <paulmabileau@hotmail.fr>
2020-12-24 02:20:22 +0100

    AoC D8 doc
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- advent-of-code/2020/rust/day8/src/bin/part1.rs
+++ advent-of-code/2020/rust/day8/src/bin/part1.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use day8::{parse_instructions, Evaluator};
 
+/// Solves part 1 by parsing the instructions and running them until a loop is found.
 fn main() -> Result<()> {
     println!(
         "{}",
--- advent-of-code/2020/rust/day8/src/bin/part2.rs
+++ advent-of-code/2020/rust/day8/src/bin/part2.rs
@@ -1,17 +1,21 @@
 use anyhow::Result;
 use day8::{parse_instructions, Evaluator, Instruction, Operation};
 
+/// Solves part 2 by traversing all the instructions, inverting `nop`s and `jmp`s,
+/// and checking if that fixes the code by trying executing it until a loop.
 fn main() -> Result<()> {
     let inss = parse_instructions()?;
 
     for (pc, ins) in inss.iter().enumerate() {
         match ins.op {
             Operation::Nothing => {
+                // Don't invert zero `nop`s as `jmp +0` results in a loop.
                 if ins.arg != 0 && print_fixed_acc(&inss, Operation::Jump, pc) {
                     break;
                 }
             }
             Operation::Jump => {
+                // Finish as soon as one inversion fixes the code.
                 if print_fixed_acc(&inss, Operation::Nothing, pc) {
                     break;
                 }
@@ -23,6 +27,9 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+/// Clones `inss`, assigns `op` to the instruction at index `pc` and runs the
+/// evaluation. If it results in executing the program until the very last
+/// instruction, then prints the accumulator and returns `true`, else `false`.
 fn print_fixed_acc(inss: &[Instruction], op: Operation, pc: usize) -> bool {
     let mut fixed_inss = inss.to_vec();
     fixed_inss[pc].op = op;
