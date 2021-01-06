b35fb773cebc069967860e03ae4c2c819f169b5d
Paul Mabileau <paulmabileau@hotmail.fr>
2021-01-06 19:43:14 +0100

    AoC D9 doc
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- advent-of-code/2020/rust/day9/src/bin/part1.rs
+++ advent-of-code/2020/rust/day9/src/bin/part1.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use day9::{find_not_sum, parse_numbers};
 
+/// Solves part 1 by finding the desired first.
 fn main() -> Result<()> {
     println!("{}", find_not_sum(&parse_numbers()?));
     Ok(())
--- advent-of-code/2020/rust/day9/src/lib.rs
+++ advent-of-code/2020/rust/day9/src/lib.rs
@@ -1,8 +1,10 @@
 use std::io::{self, BufRead};
 use anyhow::Result;
 
+/// The length of the """encryption""" preamble.
 pub const PREAMBLE_SIZE: usize = 25;
 
+/// Parses all the numbers from the standard input at once.
 pub fn parse_numbers() -> Result<Vec<u64>> {
     io::stdin()
         .lock()
@@ -13,6 +15,7 @@ pub fn parse_numbers() -> Result<Vec<u64>> {
         })
 }
 
+/// Returns the first number not a sum of two from the preamble.
 pub fn find_not_sum(numbers: &[u64]) -> u64 {
     numbers
         .windows(PREAMBLE_SIZE)
@@ -22,6 +25,7 @@ pub fn find_not_sum(numbers: &[u64]) -> u64 {
         .unwrap()
 }
 
+/// Tests whether two numbers exist in `buf` summing to `target`.
 fn find_sum(buf: &[u64], target: u64) -> bool {
     buf.iter().any(|&n| buf.contains(&target.saturating_sub(n)))
 }
